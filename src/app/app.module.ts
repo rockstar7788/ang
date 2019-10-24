999750eb1be551cf6ef96db75f56c2291bc58175
группы стационара <st@itstep.lan>
2019-10-24 21:26:41 +0300

    angular-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticlePreviewComponent } from './article-preview/article-preview.component';
+import { ArticlePreviewListComponent } from './article-preview-list/article-preview-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+
+const appRoutes = [
+  { path: '', component: ArticlePreviewComponent },
+  { path: 'test',      component: ArticlePreviewListComponent },
+  { path: 'test/:id',  component: ArticleComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlePreviewComponent,
+    ArticlePreviewListComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
